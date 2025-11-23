157d6c436b566bd3d7dedeb94c1dfbe968bc1253
Abel Arreola Flores <arreolaabel01@gmail.com>
2025-11-23 17:12:56 -0600

    Update calculo_edad.cpp

--- calculo_edad.cpp
+++ calculo_edad.cpp
@@ -4,19 +4,16 @@ using namespace std;
 int main() {
     string nombre;
     int edad;
-
     // Pedir nombre y edad
     cout << "Ingresa tu nombre: ";
     getline(cin, nombre);
     cout << "Ingresa tu edad: ";
     cin >> edad;
-
-    // Verificar si es mayor o menor de edad y mostrar mensaje
+   // Se abre decicion para determinar si es mayor o menor de edad
     if (edad >= 18) {
         cout << "Bienvenido " << nombre << ", eres mayor de edad." << endl;
     } else {
         cout << "Bienvenido " << nombre << ", eres menor de edad." << endl;
     }
-
     return 0;
 }
